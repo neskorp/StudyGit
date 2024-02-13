c2b7aec8b12aed94fefee94564745c97b11c7e9c
Павел Нескородьев <nps@rosa-it.ru>
2024-02-13 13:00:34 +0400

    Revert "Добавлено действие LinkDocument"
    This reverts commit 13756ab31dc296bedf961bd6fba04a515682ca33.

--- DirRX.HelpDesk/DirRX.HelpDesk.ClientBase/TypeRequest/TypeRequestActions.cs
+++ DirRX.HelpDesk/DirRX.HelpDesk.ClientBase/TypeRequest/TypeRequestActions.cs
@@ -7,18 +7,4 @@ using DirRX.HelpDesk.TypeRequest;
 
 namespace DirRX.HelpDesk.Client
 {
-  partial class TypeRequestActions
-  {
-    public virtual void LinkDocument(Sungero.Domain.Client.ExecuteActionArgs e)
-    {
-      
-    }
-
-    public virtual bool CanLinkDocument(Sungero.Domain.Client.CanExecuteActionArgs e)
-    {
-      return true;
-    }
-
-  }
-
 }
